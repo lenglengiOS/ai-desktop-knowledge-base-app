174bbe62f03a0058ad274f60a35e8ba4f00195e3
冷洪林 <18708363472@qq.com>
2025-01-31 21:53:57 +0800

    集成react-router路由，并将根页面铺满全屏

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,27 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+function About() {
+  return <h2>About Page</h2>;
+}
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 };
 
 const root = createRoot(document.body);
