19f34f0d1954dabf07068007f5c18588576e925d
冷洪林 <18708363472@qq.com>
2025-01-31 21:41:08 +0800

    集成antdx

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return <div></div>;
+};
+
+const root = createRoot(document.body);
+root.render(<App />);
--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,19 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("my_app_name", {
+  // 最小化
+  minimize: () => {
+    ipcRenderer.send("minimize");
+  },
+  // 最大化
+  maximize: () => {
+    ipcRenderer.send("maximize");
+  },
+  // 选择文件
+  chooseFile: async () => {
+    const path = await ipcRenderer.invoke("choose-file");
+    return path;
+  },
+});
