3b938ada2eb9d99526c1bb3f4d655239935cb6db
lenglengiOS <18708363472@qq.com>
2025-02-08 21:18:08 +0800

    配置package.json版本包读取

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,23 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor } from "./store/index";
+import Package from "../package.json";
 import "./app.css";
-import { Provider as KProvider, KeepAlive } from "react-keep-alive";
 
 // 路由
 import Home from "./pages/home/home";
 import KnowledgeDetail from "./pages/knowledgeDetail/knowledgeDetail";
 
 const App = () => {
+  useEffect(() => {
+    // 读取并回显版本号
+    const a = document.getElementById("title");
+    a.innerHTML = `海豚AI助手（版本号：${Package.version}）`;
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
