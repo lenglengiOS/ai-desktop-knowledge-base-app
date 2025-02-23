1d7138a6810e5003e6823a2b56e3f56fb9a4e78f
lenglengiOS <18708363472@qq.com>
2025-02-23 16:21:16 +0800

    增加深度思考和联网搜索布局

--- src/components/sender/sender.tsx
+++ src/components/sender/sender.tsx
@@ -1,6 +1,20 @@
 import { Sender, Bubble, BubbleProps } from "@ant-design/x";
-import { App, Flex, message, Typography } from "antd";
+import {
+  App,
+  Button,
+  Flex,
+  message,
+  Typography,
+  ConfigProvider,
+  Space,
+} from "antd";
 import React, { useState, FC, useEffect } from "react";
+import {
+  AntDesignOutlined,
+  ChromeOutlined,
+  OpenAIOutlined,
+} from "@ant-design/icons";
+import { createStyles } from "antd-style";
 
 interface Iprops {
   onSubmit?: (content: string) => void;
@@ -8,8 +22,22 @@ interface Iprops {
   loading: boolean;
 }
 
+type ButtonType = "dashed" | "link" | "default" | "text" | "primary";
+
 const LHLSender: FC<Iprops> = ({ onSubmit, onCancel, loading }) => {
   const [value, setValue] = useState<string>("");
+  const [thinkType, setThinkType] = useState<ButtonType>("dashed");
+  const [netType, setNetType] = useState<ButtonType>("dashed");
+  const { styles } = useStyle();
+
+  const onThinkClick = () => {
+    setThinkType((pre) => (pre === "dashed" ? "primary" : "dashed"));
+  };
+
+  const onNetClick = () => {
+    setNetType((pre) => (pre === "dashed" ? "primary" : "dashed"));
+  };
+
   return (
     <Flex vertical gap="middle">
       <Sender
@@ -28,8 +56,60 @@ const LHLSender: FC<Iprops> = ({ onSubmit, onCancel, loading }) => {
           onCancel && onCancel();
         }}
       />
+      <ConfigProvider
+        button={{
+          className: styles.linearGradientButton,
+        }}
+      >
+        <Space>
+          <Button
+            onClick={onThinkClick}
+            style={{ borderRadius: 16 }}
+            size="middle"
+            type={thinkType}
+            icon={<OpenAIOutlined />}
+          >
+            深度思考
+          </Button>
+          <Button
+            onClick={onNetClick}
+            style={{ borderRadius: 16 }}
+            icon={<ChromeOutlined />}
+            type={netType}
+            size="middle"
+          >
+            联网搜索
+          </Button>
+        </Space>
+      </ConfigProvider>
     </Flex>
   );
 };
 
 export default LHLSender;
+
+const useStyle = createStyles(({ prefixCls, css }) => ({
+  linearGradientButton: css`
+    &.${prefixCls}-btn-primary:not([disabled]):not(
+        .${prefixCls}-btn-dangerous
+      ) {
+      > span {
+        position: relative;
+      }
+
+      &::before {
+        content: "";
+        background: linear-gradient(135deg, #6253e1, #04befe);
+        position: absolute;
+        inset: -1px;
+        opacity: 1;
+        transition: all 0.3s;
+        border-radius: inherit;
+      }
+
+      &:hover::before {
+        opacity: 0;
+      }
+    }
+  `,
+}));
